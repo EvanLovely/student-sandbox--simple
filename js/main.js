3c845929dea050b069718afb7e6c5381ed773fc9
Evan Lovely <evan@evanlovely.com>
2015-07-09 19:24:07 -0700

    removing jQuery dependency

--- js/main.js
+++ js/main.js
@@ -1,21 +1,15 @@
-$(document).ready(function () {
-
-  // jQuery goes here
-  //$('html').addClass('js');
-
-});
-
+// We can't do anything with JavaScript until all of the HTML is loaded, so we listen for the event "DOMContentLoaded" to fire, then we can kick off the rest of our JavaScript inside the `function`.
 document.addEventListener('DOMContentLoaded', function () {
-  // All JS that needs to fire after the DOM has loaded goes here
-});
 
-// nav toggle
-document.querySelector('.js-nav-toggle').addEventListener('click', function () {
+  // nav toggle
+  document.querySelector('.js-nav-toggle').addEventListener('click', function () {
 
-  if (document.body.getAttribute('data-nav-open') === 'yes') {
-    document.body.setAttribute('data-nav-open', 'no');
-  } else {
-    document.body.setAttribute('data-nav-open', 'yes');
-  }
+    if (document.body.getAttribute('data-nav-open') === 'yes') {
+      document.body.setAttribute('data-nav-open', 'no');
+    } else {
+      document.body.setAttribute('data-nav-open', 'yes');
+    }
+
+  });
 
 });
